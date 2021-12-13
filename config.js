70e1cc3aed869001532e3d2d5a42fad9d6c26dd2
mimidaichuys <96059527+mimidaichuys@users.noreply.github.com>
2021-12-13 20:03:21 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
     titleWeb: "hehe :>",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    introTitle: 'em rảnh quá í',
+    introDesc: `nên phá, nghịch chơi`,
+    btnIntro: 'hì :Đ',
+    title: 'haizz, chả có chi làm í ;-;',
+    desc: 'nên anh giới thiệu em bộ nào mà movie ii',
+    btnYes: '"tên phim" :v',
+    btnNo: 'ko :v',
+    question: 'giờ em muốn thể loại gì á?',
+    btnReply: 'xàm ghê á :v',
+    reply: 'buồn chút, đau tim chút :D',
+    mess: 'Cảm ơn anh nhaaa',
+    messDesc: 'hìii :Đ',
+    btnAccept: 'ngáo ghê :vv',
+    messLink: 'https://github.com/zukahai/hiii' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
